6b3766f7cc72333711a67e2c91f4b392058d6e17
Shannon Griswold <shannon.v.griswold@gmail.com>
2026-08-25 17:31:12 -0400

    Upgrade to ROS Lyrical

--- .vscode/install_dependencies.sh
+++ .vscode/install_dependencies.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Update ROS dependencies
-. /opt/ros/jazzy/setup.sh && rosdep update
+. /opt/ros/lyrical/setup.sh && rosdep update
 sudo apt-get update -y
 
 # Installs ROS dependencies
-. /opt/ros/jazzy/setup.sh && export PIP_BREAK_SYSTEM_PACKAGES=1 && rosdep install --from-paths src --ignore-src -r -y
+. /opt/ros/lyrical/setup.sh && export PIP_BREAK_SYSTEM_PACKAGES=1 && rosdep install --from-paths src --ignore-src -r -y
 
 # Deletes ROS build directories
 rm -rf build install log
--- src/surface/flight_control/setup.py
+++ src/surface/flight_control/setup.py
@@ -23,7 +23,9 @@ setup(
     maintainer_email='bwp18@case.edu',
     description='Mate ROV sub movement controllers',
     license='Apache License 2.0',
-    tests_require=['pytest'],
+    extras_require={
+        'test': ['pytest'],
+    },
     entry_points={
         'console_scripts': [
             'mavlink_control_node = flight_control.mavlink_control_node:main',
